ee032ef3e5742623b2ab8f489d3a67204a5c4a12
goktug <goktug.berberoglu@tum.de>
2026-06-13 18:36:26 +0200

    update

--- app.py
+++ app.py
@@ -1,51 +1,75 @@
 import streamlit as st
 import pandas as pd
 import os
+from datetime import date
 
-
+def clear_form():
+    st.session_state["company"] = ""
+    st.session_state["position"] = ""
+    st.session_state["salary"] = ""
+    st.session_state["home_office"] = None
+    st.session_state["status"] = None
+    st.session_state["notes"] = ""
+    st.session_state["application_date"] = date.today()
 
 
 
 st.title("Job Application Tracker")
 
-st.subheader("Application Details")
+title_colunm, button_column = st.columns([4,1])
 
-company = st.text_input("Company Name")
-position = st.text_input("Position")
-salary = st.text_input("Salary")
-home_office = st.selectbox(
-    "Home Office", ["Yes", "No", "?"], index=None, placeholder="Select")
-application_date = st.date_input("Application Date")
-notes = st.text_area("Notes")
+with  title_colunm:
+    st.subheader("Application Details")
+with button_column:
+    st.button("Clear All", on_click=clear_form)
 
-status = st.selectbox(
-    "Application Status", ["Applied", "Interview", "Rejected", "Offer"])
 
-if st.button("Submit Application"):
+with st.form("application_form", clear_on_submit=True):
+    company = st.text_input("Company Name", key="company")
+    position = st.text_input("Position", key="position")
+    salary = st.text_input("Salary", key="salary")
+    home_office = st.selectbox(
+        "Home Office", ["Yes", "No", "?"], index=None, placeholder="Select", key = "home_office")
+    application_date = st.date_input("Application Date", key="application_date")
+    notes = st.text_area("Notes",key="notes")
+    status = st.selectbox(
+        "Application Status", ["Applied", "Interview", "Rejected", "Offer"], key="status")
+    submitted = st.form_submit_button("Submit Application")
 
-    new_application = pd.DataFrame([{
-        "Company": company,
-        "Position": position,
-        "Salary": salary,
-        "Home Office": home_office,
-        "Status": status
-    }])
+if submitted:
 
-    if os.path.exists("applications.csv"):
-        new_application.to_csv(
-            "applications.csv",
-            mode="a",
-            header=False,
-            index=False
-        )
-
+    if not company.strip():
+        st.warning("Company Name required")
+    elif not position.strip():
+        st.warning("Position Name required")
+    elif status is None:
+        st.warning("Status required")
     else:
-        new_application.to_csv(
-            "applications.csv",
-            index=False
-        )
+        new_application = pd.DataFrame([{
+            "Company": company,
+            "Position": position,
+            "Salary": salary,
+            "Home Office": home_office,
+            "Status": status,
+            "Application Date": application_date,
+            "Notes": notes
+        }])
 
-    st.success("Application saved.")
+        if os.path.exists("applications.csv"):
+            new_application.to_csv(
+                "applications.csv",
+                mode="a",
+                header=False,
+                index=False
+            )
+
+        else:
+            new_application.to_csv(
+                "applications.csv",
+                index=False
+            )
+
+        st.success("Application saved.")
 
 st.subheader("Saved Applications")
 if st.button("View Applications"):
--- pages/1_Applications.py
+++ pages/1_Applications.py
@@ -6,13 +6,34 @@ st.title("Applications")
 
 if os.path.exists("applications.csv"):
     applications = pd.read_csv("applications.csv")
-    edited_applications = st.data_editor(
-        applications,
-        use_container_width=True,
-        hide_index=True
+
+    status_options = ["All"] + sorted(
+        applications["Status"].dropna().unique().tolist()
     )
+
+    selected_status = st.selectbox("Filter by Status", status_options, index=0)
+
+    filtered_applications = applications.copy()
+
+    if selected_status != "All":
+        filtered_applications = applications[
+            applications["Status"] == selected_status
+        ].copy()
+    if selected_status == "All":
+        edited_applications = st.data_editor(
+            filtered_applications,
+            use_container_width=True,
+            hide_index=True,
+            num_rows="dynamic"
+        )
+    else:
+        st.dataframe(filtered_applications,
+                     width="stretch", hide_index=True)
+        
+                
     if st.button("Save Changes"):
         edited_applications.to_csv("applications.csv", index=False)
         st.success("Changes saved successfully.")
+    
 else:
     st.info("No applications have been saved yet.")
